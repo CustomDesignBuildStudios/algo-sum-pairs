6b8d2518b0a0fccd6c6eba551a53241459757062
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 21:50:12 +0200

    Added python functions

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,10 @@
-def sum_pairs():
+def sum_pairs(array_numbers, sum_number):
+    array_of_sums = []
+    for first_number in range(len(array_numbers)):
+        for second_number in range(first_number,len(array_numbers),1):
+            if array_numbers[first_number] + array_numbers[second_number] == sum_number:
+                array_of_sums.append([array_numbers[first_number],array_numbers[second_number]])
+    if len(array_of_sums) > 0:
+        return array_of_sums
+    else:
+        return 'unable to find pairs'
